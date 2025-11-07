8bdb482aef7066d1a9f5119c68f3d741341a50f3
Ayoub Hadi <AyoubHadi856@gmail.com>
2025-11-07 12:23:26 +0100

    Delete Java_challenges problem-solving directory

--- Java_challenges problem-solving/.idea/Java_challenges_problem-solving.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import java.lang.Math;
-import java.util.Arrays;
-public class Java_challanges_problem-solving{
-    public static void main(String[] args) {
-
-    }
-}
-
