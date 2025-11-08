26338569161d684072ff0924f720730ab461f69d
Ayoub_Hadi <Ayoubhadi856@gmail.com>
2025-11-08 19:53:48 +0100

    update stock

--- VendingMachineJava/src/Main.java
+++ VendingMachineJava/src/Main.java
@@ -1,13 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
+import java.io.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class VendingMachine {
+
+public static void main(String[] args) {
+ArrayList<String> produits = new ArrayList<>(Arrays.asList("Eau","Soda","Chips","Chocolat"));
+ArrayList<Integer> prix = new ArrayList<>(Arrays.asList(5, 8, 12, 15)); // use ints to keep it simple
+ArrayList<Integer> stock = new ArrayList<>(Arrays.asList(10, 5, 7, 3));
+Scanner scanner = new Scanner(System.in);
+while(true) {
+    acheterProduit(produits, prix, stock, scanner);
+    System.out.println("Updated stock: " + stock);
+}
     }
+    public static void acheterProduit(ArrayList<String> produits,ArrayList<Integer> prix,ArrayList<Integer> stock,Scanner scanner) 
+    {
+        System.out.println("saiser le number de produit : ");
+        int n = scanner.nextInt();
+        int index=n-1;
+       
+
+    String produit_Nom =  produits.get(index);
+    int produit_prix  =  prix.get(index);
+    int produit_Stock =  stock.get(index);
+
+
+if ( produit_Stock > 0 ) {
+        System.out.println("saiser prix : ");
+        int user_prix = scanner.nextInt();
+    if (user_prix == produit_prix ) {
+        stock.set(index, produit_Stock - 1);
+        System.out.println("you have bought : "+produit_Nom);
+    }else if (user_prix < produit_prix){
+        System.out.println("the produit cost more money that this : "+ user_prix);
+    }
+    else if (user_prix > produit_prix){
+        int return_change=user_prix-prix.get(index);
+        System.out.println("You have bought: " + produit_Nom);
+        stock.set(index, produit_Stock - 1);
+        System.out.println("Change returned: " + return_change + " MAD");
+    }  
+    }
+    else{
+    System.out.println("Product out of stock.");
+}
+        
+
+}
+
 }
